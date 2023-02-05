efb935f5341dac936f6a7d9f753270b24188a659
Nathan <nathanacuevas97@gmail.com>
2023-02-04 16:30:37 -0800

    Added Username Updating functionality in the profile settings modal.

--- src/components/styled/elements/Input/index.js
+++ src/components/styled/elements/Input/index.js
@@ -4,8 +4,8 @@ import * as S from "./Input.styles";
 function Index({ type, name, label, size, ...rest }) {
   return (
     <S.Label className={`${size}`} htmlFor={name}>
-      <S.LabelSpan>{label}</S.LabelSpan>
       <S.Input type={type} name={name} {...rest} />
+      <S.LabelSpan>{label}</S.LabelSpan>
     </S.Label>
   );
 }
